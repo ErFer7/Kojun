bcd951d689625eb2fb025737a4fa97df11313e25
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2022-05-22 21:45:45 -0300

    Plano adicionado

--- KojunSolver.hs
+++ KojunSolver.hs
@@ -7,6 +7,14 @@
 -- Resolução
 -- Exibição do resultado
 
+-- TODO:
+-- v0.2: Parser e Structure    (2022-05-23)
+-- v0.3: Printer               (2022-05-25)
+-- v0.4: Solver                (2022-05-25)
+-- v0.5: Aprimorar o algoritmo (2022-05-26)
+-- v1.0: Arrumar tudo          (2022-05-27)
+-- Relatório                   (2022-05-28)
+
 import Structures
 
 
