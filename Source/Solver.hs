d124dd96094393b4157846b17b1f113c99aa4df7
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2022-05-26 10:05:42 -0300

    Alterações básicas

--- Source/Solver.hs
+++ Source/Solver.hs
@@ -18,6 +18,19 @@ module Solver where
 
 import Structure
 
+-- Auxiliares ------------------------------------------------------------------
+-- count :: Int -> [Int] -> Int
+-- count [] v = 0
+-- count (a:b) v
+--     | a == v = 1 + count b v
+--     | otherwise = count b v
+
+-- checkRegionValues :: [Int] -> Bool
+-- checkRegionValues [] = True
+-- checkRegionValues (a:b)
+--     | count ((length (a:b)) (a:b)) == 1 = checkRegion b
+--     | otherwise = False
+
 {-
 removeElement :: Int -> [Int] -> [Int]
 removeElement n [] = []
