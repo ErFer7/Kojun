96c4cd20e14d8846b9eff5f5bdf18e53f4f4225f
yokiyoki87 <someyokisoba@gmail.com>
2022-07-17 21:44:47 -0300

    funcoes aux get_cell

--- Prolog/Source/Solver.pl
+++ Prolog/Source/Solver.pl
@@ -7,24 +7,33 @@
 
 % Solução ---------------------------------------------------------------------
 
+% auxiliares de pegar valores
+get_cell_value(Puzzle,X,Y,V):-
+    get_cell(Puzzle,X,Y,[V,_]).
+
+get_cell_region(Puzzle,X,Y,R):-
+    get_cell(Puzzle,X,Y,[_,R]).
+
+get_cell(Puzzle,X,Y,[V,R]):-
+    nth0(Y, Puzzle, Line),
+    nth0(X, Line, [V, R]).
+
+% verifica se dentre uma regiao, uma celula eh maior que a inferior
 vertical_greatness(Puzzle, X, Y) :-
     %
     length(Puzzle, Size),
     % Obtém a célula na posição (x, y)
-    nth0(Y, Puzzle, Line),
-    nth0(X, Line, [V, R]),
+    get_cell(Puzzle,X,Y,[V,R]),
     % Obtém a célula acima
     (   Y > 0
         ->  Yup is Y - 1,  % Yup
-        nth0(Yup, Puzzle, LineAbove),
-        nth0(X, LineAbove, [Vup, Rup])
+        get_cell_region(Puzzle,X,Yup,Rup)
     ;   Rup = -1
     ),
     % Obtém a célula abaixo
     (   Y < (Size - 1)
     ->  Ydown is Y + 1,
-        nth0(Ydown, Puzzle, LineUnder),
-        nth0(X, LineUnder, [Vdown, Rdown])
+        get_cell_region(Puzzle,X,Ydown,Rdown)
     ;   Rdown = -1
     ),
     % Regras de verificação
@@ -64,66 +73,39 @@ solve(Puzzle) :-
 
 % % funcoes para manter valores entre 1 e N
 
-% % conta elementos em regiao
-% elemsInRegion(_,[],C):- C = 0.
-% elemsInRegion(R,[[_,R]|T],C):- elemsInRegion(R,T,C1), C = C1+1.
-% elemsInRegion(R,[_|T],C):- elemsInRegion(R,T,C).
 
-% % verifica se todos estao em 1:N em uma regiao
-% allOneThroughN([],_).
-% allOneThroughN([[V1,R1]|T],L):- elemsInRegion(R1,L,C),
-%                                 V1 #=< C,
-%                                 V1 #>= 1,
-%                                 allOneThroughN(T,L).
+below_N(Puzzle,X,Y):- 
+    get_cell(Puzzle,X,Y,[V,R]),
+    get_region(Puzzle,R). % continua...
+    
 
-% % aplica funcao acima a partir de matriz original
-% allBelowN(P):- allOneThroughN(flatten(P),flatten(P)).
-
-
-% % funcoes para evitar valor maior que a posicao inferior
-
-% % checa se valor de um elemento eh maior q o proximo se for mesma regiao
-% checkColumn([_]).
-% checkColumn([[V1,R],[V2,R]|T]):- V1 #> V2, checkColumn([[V2,R]|T]).
-% checkColumn([_|T]):- checkColumn([T]).
-
-% % aplica funcao acima a cada coluna
-% vgAux([C]):- checkColumn(C).
-% vgAux([C|T]):- checkColumn(C), vgAux(T).
-
-% % aplica checagem para a transposta, checando coluna e nao linha
-% verticalGreatness(M):- vgAux(transpose(M)).
-
-
+% verifica diferenca entre casas adjacentes
 orthogonal_difference(Puzzle,X,Y) :-
     length(Puzzle,Size),
     % Pega celula na posicao (X,Y)
-    nth0(Y,Puzzle,Line),
-    nth0(X,Puzzle,Cell),
+    get_cell_value(Puzzle,X,Y,V)
     % Acima
     ( Y > 0
     -> Yup = Y - 1,
-    nth0(Yup,Puzzle,LineAbove),
-    nth0(X,LineAbove,[Vup,Rup])
+    get_cell_value(Puzzle,X,Yup,Vup)
     ; Vup = -1
     ),
     % Abaixo
     ( Y < (Size - 1)
     -> Ydown = Y + 1,
-    nth0(Ydown,Puzzle,LineBelow),
-    nth0(X,LineBelow,[Vdown,Rdown])
+    get_cell_value(Puzzle,X,Ydown,Vdown)
     ; Vdown = -1
     ),
     % Esquerda
     ( X > 0
     -> XLeft = X - 1,
-    nth0(XLeft,Line,[Vleft,Rleft])
+    get_cell_value(Puzzle,XLeft,Y,Vleft)
     ; Vleft = -1
     ),
     % Direita
     ( X < (Size - 1),
     XRight = X + 1,
-    nth0(XRight,Line,[Vright,Rright])
+    get_cell_value(Puzzle,XRight,Y,Vright)
     ; Vright = -1
     ),
     % Regras
